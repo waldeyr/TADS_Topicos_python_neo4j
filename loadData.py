692b657c39a98dcefb92fa1f668739148e6689c0
wmcs <mendes@iscb.org>
2018-08-31 09:14:40 -0300

    transormado em classe; separado em metodo para varga no banco de dados;

--- loadData.py
+++ loadData.py
@@ -3,20 +3,28 @@
 from py2neo import *
 import pandas as pd
 
-g = Graph(password="password")
+class TopicosI:
 
-df = pd.read_csv('data.txt', sep=',')
-print(df)
-for index, linha in df.iterrows():
-	tx = g.begin()
-	p = Node("Pessoa", nome=linha['pessoa'])
-	h = Node("Hobbie", nome=linha['hobbie'])
-	t = Node("Profissao", nome=linha['profissao'])
-	tx.create(p)
-	tx.create(h)
-	tx.create(t)
-	p_h = Relationship(p, "GOSTA_DE", h)
-	p_t = Relationship(p, "QUER_SER", t)
-	tx.create(p_h)
-	tx.create(p_t)
-	tx.commit()
+	GRAPH = Graph(password="ifg")
+
+	def loadDatabase(self):
+		df = pd.read_csv('data.txt', sep=',')
+		print(df)
+		for index, linha in df.iterrows():
+			tx = self.GRAPH.begin()
+			p = Node("Pessoa", nome=linha['pessoa'])
+			h = Node("Hobbie", nome=linha['hobbie'])
+			t = Node("Profissao", nome=linha['profissao'])
+			tx.merge(p)
+			tx.merge(h)
+			tx.merge(t)
+			p_h = Relationship(p, "GOSTA_DE", h)
+			p_t = Relationship(p, "QUER_SER", t)
+			tx.create(p_h)
+			tx.create(p_t)
+			tx.commit()
+
+
+topicos  = TopicosI();
+
+topicos.loadDatabase()
