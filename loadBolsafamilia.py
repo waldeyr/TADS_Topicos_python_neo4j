8ebb99aa8c58c524823b210bff3fe9ab96e8c178
Waldeyr Mendes Cordeiro da Silva <waldeyr@users.noreply.github.com>
2018-09-21 10:44:37 -0300

    Load bolsa familia for computers with low memory;

---
+++ loadBolsafamilia.py
@@ -0,0 +1,32 @@
+# sudo apt-get install python3-pip
+# sudo python3 -m pip install pip --upgrade
+# sudo pip install pandas
+# sudo python3 -m pip install py2neo
+import pandas as pd
+from py2neo import *
+
+
+class BolsaFamilia:
+	GRAPH = Graph(password="ifg")
+
+	def cleanDatabase(self):
+		self.GRAPH.run("MATCH (n) DETACH DELETE n;")
+
+	def loadDatabase(self):
+		for dataframe in pd.read_csv('BF201808.csv', sep=";", chunksize=10 ** 6):
+			for index, linha in dataframe.iterrows():
+				tx = self.GRAPH.begin()
+			p = Node("Pagamento", valor=linha['VALOR PARCELA'], mesReferencia=linha['MÊS REFERÊNCIA'],
+					 mesCompetencia=linha['MÊS COMPETÊNCIA'])
+			h = Node("Beneficiário", nome=linha['NOME FAVORECIDO'], nis=linha['NIS FAVORECIDO'], uf=linha['UF'],
+					 codMunicipio=linha['CÓDIGO MUNICÍPIO SIAFI'], nomeMunicipio=linha['NOME MUNICÍPIO'])
+			tx.create(p)
+			tx.merge(h, "Beneficiário", "NIS FAVORECIDO")
+			p_h = Relationship(h, "RECEBE", p)
+			tx.create(p_h)
+			tx.commit()
+
+
+bf = BolsaFamilia()
+bf.cleanDatabase()
+bf.loadDatabase()
